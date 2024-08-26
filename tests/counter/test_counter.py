1fdb235773fb6d812f1eea07f6061560f1bff3c4
CamilaGon <cg561351@gmail.com>
2024-08-26 19:57:42 -0300

    Inicia, req1

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.pre_built.counter import count_ocurrences
-
+from src.pre_built.counter import count_ocurrences
 
 def test_counter():
-    raise
+    path = "data/jobs.csv"
+
+    assert count_ocurrences(path, "Python") == 1639, "A contagem para 'Python' está incorreta."
+    assert count_ocurrences(path, "JavaScript") == 122, "A contagem para 'JavaScript' está incorreta."
