b58c2ff7d61114261697c84d5c4fa2f66af78ac2
CamilaGon <cg561351@gmail.com>
2024-08-26 20:01:58 -0300

    corrigi lint

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,7 +1,8 @@
 from src.pre_built.counter import count_ocurrences
 
+
 def test_counter():
     path = "data/jobs.csv"
-
+    
     assert count_ocurrences(path, "Python") == 1639, "A contagem para 'Python' está incorreta."
     assert count_ocurrences(path, "JavaScript") == 122, "A contagem para 'JavaScript' está incorreta."
